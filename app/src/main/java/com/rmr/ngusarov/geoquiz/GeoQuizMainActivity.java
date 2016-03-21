f38e5079ea13179c16ee00d6fb3a9c74f8b22054
Abbath0767 <ng@redmadrobot.com>
2016-03-21 12:37:20 +0300

    bug fixed

--- app/src/main/java/com/rmr/ngusarov/geoquiz/GeoQuizMainActivity.java
+++ app/src/main/java/com/rmr/ngusarov/geoquiz/GeoQuizMainActivity.java
@@ -38,6 +38,7 @@ public class GeoQuizMainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 counter++;
+                counter%=questArr.length;
                 refreshQuest();
             }
         });
