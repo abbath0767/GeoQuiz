7fdf992d8ea3503cc903c3e71e66e1092a772574
Abbath0767 <ng@redmadrobot.com>
2016-03-22 19:49:36 +0300

    add layout-land, backButton and saveInstanceState, need debug

--- app/src/main/java/com/rmr/ngusarov/geoquiz/GeoQuizMainActivity.java
+++ app/src/main/java/com/rmr/ngusarov/geoquiz/GeoQuizMainActivity.java
@@ -2,6 +2,7 @@ package com.rmr.ngusarov.geoquiz;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -9,9 +10,13 @@ import android.widget.Toast;
 
 public class GeoQuizMainActivity extends AppCompatActivity {
 
+    public static final String TAG = "my debug tag";
+    public static final String KEY_INDEX = "key_index";
+
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
+    private Button mBackButton;
     private TextView mTextView;
     private static int counter;
     private static TrueFalse[] questArr;
@@ -19,9 +24,13 @@ public class GeoQuizMainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, ">> on create <<");
         setContentView(R.layout.activity_geo_quiz_main);
 
-        counter = 0;
+        if (savedInstanceState == null)
+            counter = 0;
+        else
+            counter = savedInstanceState.getInt(KEY_INDEX);
 
         questArr = new TrueFalse[]{new TrueFalse(R.string.russia_question1, true),
                 new TrueFalse(R.string.russia_question2, false),
@@ -39,6 +48,16 @@ public class GeoQuizMainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 counter++;
                 counter%=questArr.length;
+                refreshQuest();
+            }
+        });
+
+        mBackButton = (Button) findViewById(R.id.back_button);
+        mBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+        public void onClick(View v) {
+                counter--;
+                counter = counter < 0 ? questArr.length : counter;
                 refreshQuest();
             }
         });
@@ -65,15 +84,49 @@ public class GeoQuizMainActivity extends AppCompatActivity {
     }
 
     private void validateAnswer(View v) {
+        int answerId;
         if (questArr[counter].isQuestionResult() && v.getId() == R.id.true_button)
-            Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+            answerId = R.string.correct_toast;
         else if (!questArr[counter].isQuestionResult() && v.getId() == R.id.false_button)
-            Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+            answerId = R.string.correct_toast;
         else
-            Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+            answerId = R.string.incorrect_toast;
+
+        Toast.makeText(this, answerId, Toast.LENGTH_SHORT).show();
     }
 
-//    @Override
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        Log.d(TAG, ">> save instance state coming, save int = " + counter);
+        super.onSaveInstanceState(outState);
+        outState.putInt(KEY_INDEX, counter);
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, ">> on stop <<");
+        super.onStop();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(TAG, ">> on pause <<");
+        super.onPause();
+    }
+
+    @Override
+    protected void onRestart() {
+        Log.d(TAG, ">> on restart <<");
+        super.onRestart();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, ">> on destroy <<");
+        super.onDestroy();
+    }
+
+    //    @Override
 //    public boolean onCreateOptionsMenu(Menu menu) {
 //        return true;
 //    }
